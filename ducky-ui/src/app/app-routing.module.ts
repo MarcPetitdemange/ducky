f2546f89c3a0d33620d874cc66fda3b55261b9ad
Marc PETITDEMANGE <marc.petitdemange@ac-nancy-metz.fr>
2023-03-30 16:27:16 +0200

    Ajout de multiples fonctionnalités + Développement de l'interface graphique de ducky

--- ducky-ui/src/app/app-routing.module.ts
+++ ducky-ui/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientsComponent } from './component/pages/clients/clients.component';
+import { HomeComponent } from './component/pages/home/home.component';
+import { NotFoundComponent } from './component/pages/not-found/not-found.component';
+import { UsersComponent } from './component/pages/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'users', component: UsersComponent},
+  {path:'clients', component: ClientsComponent},
+  {path:'404', component: NotFoundComponent},
+  {path:'**', redirectTo: '/404'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ducky-ui/src/app/app.module.ts
+++ ducky-ui/src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { MenuComponent } from './component/menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { HomeComponent } from './component/pages/home/home.component';
+import { UsersComponent } from './component/pages/users/users.component';
+import { ClientsComponent } from './component/pages/clients/clients.component';
+import { NotFoundComponent } from './component/pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    HomeComponent,
+    UsersComponent,
+    ClientsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,   
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
